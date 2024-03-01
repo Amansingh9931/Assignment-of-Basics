8def3ba7b2ac9855db65e6bf38a6a852788c0039
Amansingh9931 <amansingh12073@gmail.com>
2024-03-01 12:12:59 +0530

    Assignment commit

---
+++ Q5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r,circumference;
+    cout<<"Enter the radius";
+    cin>>r;
+    circumference=2*3.14*r;
+    cout<<"circumference is : "<<circumference;
+
+}
